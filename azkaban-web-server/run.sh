7027887f9162614a2768eb5a3bc79eadf5b3fa9f
chenzj <junecloud@163.com>
2019-12-27 13:45:03 +0800

    修改代码

---
+++ azkaban-web-server/run.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+
+DB_LOOPS="20"
+MYSQL_HOST="mysql"
+MYSQL_PORT="3306"
+START_CMD="bin/start-web.sh"
+
+#wait for mysql
+i=0
+while ! nc $MYSQL_HOST $MYSQL_PORT >/dev/null 2>&1 < /dev/null; do
+  i=`expr $i + 1`
+  if [ $i -ge $DB_LOOPS ]; then
+    echo "$(date) - ${MYSQL_HOST}:${MYSQL_PORT} still not reachable, giving up"
+    exit 1
+  fi
+  echo "$(date) - waiting for ${MYSQL_HOST}:${MYSQL_PORT}..."
+  sleep 1
+done
+
+sleep 10
+
+# Work around to run container as a daemon
+sed -i "s/ &//" $START_CMD
+
+curl -G "executor:12321/exector?action=activate" && echo
+
+AZKABAN_OPTS="-Xmx512m"
+
+exec $START_CMD
